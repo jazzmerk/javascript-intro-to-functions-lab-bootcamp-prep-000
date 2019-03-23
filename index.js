789b5be3b005e2ea15713cae5eb6edfe749fdc16
James Merkin <jnmerk@gmail.com>
2019-03-23 19:15:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,24 @@ function logWhisper (string){
 function sayHiToGrandma (string){
   var uppercase
   var lowercase
+<<<<<<< HEAD
   var mixedcase
+=======
+>>>>>>> db310857efe1c6fd2f74cbf6232000797060caed
   uppercase=string.toUpperCase()
   if (uppercase===string)
   return ("YES INDEED!")
   lowercase=string.toLowerCase()
   if (lowercase===string)
   return ("I can't hear you!")
+<<<<<<< HEAD
   mixedcase=string
   if (mixedcase==="I love you, Grandma.")
     return ("I love you, too.")
+=======
+  var mixedcase
+  mixedcase=string
+  if (string==="I love u, Grandma.")
+  return ("I love you, too.")
+>>>>>>> db310857efe1c6fd2f74cbf6232000797060caed
   }
